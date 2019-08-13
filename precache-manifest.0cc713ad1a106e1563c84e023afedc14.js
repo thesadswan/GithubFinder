97f784b1c963c2545f9de4fd2caac55a245d3c17
Mo <mohammedfallah838@gmail.com>
2019-08-13 10:45:06 +0430

    first commit

---
+++ precache-manifest.0cc713ad1a106e1563c84e023afedc14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff1a0934615b3db0cc7e5a6219c415d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573618fea1e3ec62525c",
+    "url": "/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "ed582d1db90491be42cd",
+    "url": "/static/js/2.88e3e19e.chunk.js"
+  },
+  {
+    "revision": "573618fea1e3ec62525c",
+    "url": "/static/js/main.aa43de42.chunk.js"
+  },
+  {
+    "revision": "395cbc264d638d2ffa80",
+    "url": "/static/js/runtime~main.fb97fa2f.js"
+  }
+]);
